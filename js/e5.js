97d034985862c58397290c329438a0a5b51a1e2a
yinzn <yinzn@guahao.com>
2018-01-26 15:36:10 +0800

    E5

--- js/e5.js
+++ js/e5.js
@@ -1,6 +1,13 @@
-var e5={
-    typeEnum:[]
-
+var e5= {
+    typeEnum : []
 };
+e5.typeEnum["11"]="花式学习";
+e5.typeEnum["12"]="花式表演";
+e5.typeEnum["13"]="花式疯狂";
+e5.typeEnum["14"]="花式双板";
+e5.typeEnum["15"]="花式情侣";
+e5.typeEnum["16"]="花式混音";
 e5.typeEnum["21"]="竞速疯狂";
 e5.typeEnum["22"]="竞速双板";
+e5.typeEnum["23"]="竞速情侣";
+e5.typeEnum["24"]="竞速混音";
